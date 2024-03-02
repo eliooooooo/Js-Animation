5a87445af6f7c01a9473f9855eb55170d4f608ce
eliooooooo <eliott.burkle@gmail.com>
2024-03-02 15:48:03 +0100

    Draw land in canvas

--- TP3/script.js
+++ TP3/script.js
@@ -11,15 +11,62 @@ class Terrain {
 
     getLand(largeur, hauteur) {
         const tab = new Array(largeur);
+        let nbRocks = 20;
         for (let i = 0; i < tab.length; i++) {
         tab[i] = new Array(hauteur);
             for (let j = 0; j < tab[i].length; j++) {
-                tab[i][j] = 0;
+                if (i === 0 || i === tab.length - 1 || j === 0 || j === tab[i].length - 1) {
+                    tab[i][j] = 2;
+                } else {
+                    tab[i][j] = 0;
+                }
             }
         }
+        let k = 0;
+        while (k < nbRocks) {
+            let i = Math.floor(Math.random() * largeur);
+            let j = Math.floor(Math.random() * hauteur);
+            if (i !== 0 && i !== largeur - 1 && j !== 0 && j !== hauteur - 1) {
+                tab[i][j] = 1;
+                k++;
+            }
+        }
+
         return tab;
+    }
+
+    draw() {
+        let clientHeight = canvas.clientHeight;
+        let clientWidth = canvas.clientWidth;
+
+        let cellWidth = clientWidth / this.largeur;
+        let cellHeight = clientHeight / this.hauteur;
+
+        for (let i = 0; i < this.sol.length; i++) {
+            for (let j = 0; j < this.sol[i].length; j++) {
+                if (this.sol[i][j] === 0) {
+                    ctx.fillStyle = 'white';
+                } else if (this.sol[i][j] === 1) {
+                    ctx.fillStyle = 'black';
+                } else if (this.sol[i][j] === 2) {
+                    ctx.fillStyle = 'red';
+                }
+                ctx.fillRect(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
+            }
+        }
+    }
+
+    read(i, j) {
+        return this.sol[i][j];
+    }
+
+    write(i, j, value) {
+        this.sol[i][j] = value;
     }
 }
 
 let terrain = new Terrain(100, 100);
-console.log(terrain.sol);
+let terrainJSON = JSON.stringify(terrain.sol);
+console.log(terrainJSON);
+
+terrain.draw();
